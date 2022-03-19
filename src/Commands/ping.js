e12813a7a13b90c5afffad663f8504b7be0540af
KrishnaKsagar <74908517+KrishnaKsagar@users.noreply.github.com>
2022-03-19 20:13:47 +0530

    Add files via upload

---
+++ src/Commands/ping.js
@@ -0,0 +1,9 @@
+const Command = require("../Structures/Command.js");
+
+module.exports = new Command({
+  name: "ping",
+  description: "Shows the ping of the bot!",
+  async run(message, args, client) {
+    const msg = await message.reply(`🏓 Pong: **${Date.now() - message.createdTimestamp}ms**.`);
+  }
+});
